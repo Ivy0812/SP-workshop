d63fa16dca5f3a6cd4d18af961e74b252a68b54c
Ivy0812 <18242067976@163.com>
2021-10-04 10:41:29 +0100

    you can start step7 next

--- next 7.R
+++ next 7.R
@@ -23,7 +23,10 @@ for (p in punc) {
 #step 6
 a<-tolower(a)
 b<-unique(a)#finds the vector of unique words
-c<-match(b,a)#finds the vector of indicies
-.Machine$integer.max <- 2^40
-freq<-tabulate(nchar(c))#indicates the number of times each unique word occurs in the text
-freq
+c<-match(a,b)#finds the vector of indicies
+freq<-tabulate(c)#indicates the number of times each unique word occurs in the text
+oder_index<-order(freq,decreasing=TRUE)[1:1000]#？If we need to certify why 1000 is the most common?not 1001 
+b_index<-sort(oder_index)
+b<-b[b_index]
+b
+
