3d40ae1446d8a4955fabaae0ef95cede4f3dae7a
“Megan0420” <2207399@ed.ac.uk>
2021-10-06 19:47:49 +0800

    standarlizing A

--- next 7.R
+++ next 7.R
@@ -1,4 +1,4 @@
-setwd("H:/OneDrive - University of Edinburgh/SP/SP-workshop")
+setwd("/Users/yuwenyi/SP-workshop")
 a <- scan("1581-0.txt",what="character",skip=156)
 n <- length(a)
 a <- a[-((n-2909):n)] ## strip license
@@ -30,7 +30,8 @@ b_index<-sort(oder_index)
 b<-b[b_index]
 b
 
-#step 7
+
+#STEP 7
 d <- match(a,b) #indices of the bible words in common word vector b
 D <- cbind(d[-length(a)],d[2:length(a)]) 
 D <- D[-which(is.na(rowSums(D)) == TRUE),] #matrix storing indices of common word pairs
@@ -38,3 +39,4 @@ A <- matrix(0,length(b),length(b)) #initializing transition probility matrix A t
 for (i in 1:dim(D)[1]){
   A[D[i,1],D[i,2]] = A[D[i,1],D[i,2]] + 1
 } #count the number of each common word pair
+Standarlized_A<-A/rowSums(A)
