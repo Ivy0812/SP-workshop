5d1ec90cc3032bff81bda2eb98f092190f2645f2
Ivy0812 <18242067976@163.com>
2021-10-06 18:05:12 +0100

    edit step 6

--- 7(e).R
+++ 7(e).R
@@ -25,10 +25,10 @@ a<-tolower(a)
 b<-unique(a)#finds the vector of unique words
 c<-match(a,b)#finds the vector of indicies
 freq<-tabulate(c)#indicates the number of times each unique word occurs in the text
-oder_index<-order(freq,decreasing=TRUE)[1:1000]#？If we need to certify why 1000 is the most common?not 1001 
-b_index<-sort(oder_index)
+freq_1000<-sort(freq,decreasing=TRUE)#find that frequency of 1000th.
+order_index<-order(freq[freq>=freq_1000[1000]],decreasing=TRUE)
+b_index<-sort(order_index)
 b<-b[b_index]
-b
 
 
 #STEP 7
