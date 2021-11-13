a61440c85e7d9745efc770b34c076f01be534887
Minke-pan <s2160782@ed.ac.uk>
2021-11-13 23:55:50 +0000

    add conditions

--- struc.R
+++ struc.R
@@ -1,43 +1,46 @@
 
 
 bfgs <- function (theta,f,...,tol=1e-5,fscale=1,maxit=100){
-  
-  first <- function(theta,f,...){
-    fd <- th0 <- theta
-    nll0 <- f(theta,...) ## nll at th0
-    eps <- 1e-7 ## finite difference interval
-    for (i in 1:length(th0)) { ## loop over parameters
-      th1 <- th0; th1[i] <- th1[i] + eps
-      th1[i] <- th1[i] + eps ## increase th0[i] by eps 
-      nll1 <- f(th1,...) ## compute resulting nll
-      fd[i] <- (nll1 - nll0)/eps ## approximate -dl/dth[i]
+  if (is.infinite(f(theta,...)))  stop("   ")
+  if (is.null(attributes(f(theta,...)))){
+    first <- function(theta,f,...) {
+      fd <- th0 <- theta
+      nll0 <- f(theta,...) ## nll at th0
+      eps <- 1e-7 ## finite difference interval
+      for (i in 1:length(th0)) { ## loop over parameters
+        th1 <- th0; th1[i] <- th1[i] + eps
+        th1[i] <- th1[i] + eps ## increase th0[i] by eps 
+        nll1 <- f(th1,...) ## compute resulting nll
+        fd[i] <- (nll1 - nll0)/eps ## approximate -dl/dth[i]
+        fd
       }
-    fd
+    }
   }
-  
-  if (is.null(attributes(f(theta,...)))){ 
-    fd <- first(theta,f,...)
-  }   
   else{
-    fd <- attr(f(theta,...),"gradient")
+    first <- function(theta,f,...) {
+      fd <- attr(f(theta,...),"gradient")
+      fd
+    }
   }
-      
-  iter <- 0
+  # 1. If the objective or derivatives are not finite at the initial theta
+  if (is.infinite(first(theta,f,...))) stop("   ")
+  
   Bk1 <- diag(length(theta))
-  for (i in 1:maxit){
+  for (iter in 1:maxit){
     sk <- delta <- - Bk1 %*% first(theta,f,...)
     theta1 <- theta + delta
     yk <- first(theta1,...) - first(theta,f,...)
     pk <- 1/t(sk) %*% yk
-    Bk1 <- (diag(length(theta))- pk*sk %*% t(yk)) %*% Bk1 %*% (diag(len(th0))- pk*yk %*% t(sk)) + pk*sk %*% t(sk)
-    iter <- iter+1
+    A <- Bk1 - pk*sk %*% (t(yk) %*% Bk1)
+    Bk1 <- A - pk*(A %*% yk) %*% t(sk) + pk*sk %*% t(sk)
     if (max(abs(first(theta,f,...))) < (abs(f(theta,...))+fscale)*tol) {
-      stop()
+      stop("  ")
     }
     else{
       theta <- theta1
     }
   } 
+  
   gll0 <- first(theta,f,...) ## gran of nll at th0 
   eps <- 1e-7 ## finite difference interval
   th0 <- theta
@@ -46,10 +49,20 @@ bfgs <- function (theta,f,...,tol=1e-5,fscale=1,maxit=100){
     th1 <- th0; th1[i] <- th1[i] + eps ## increase th0[i] by eps 
     gll1 <- first(th1,f,...) ## compute resulting nll
     Hfd[i,] <- (gll1 - gll0)/eps ## approximate second derivs
-      }     
-  
-   list(f=f(theta,...), theta=theta, iter=iter, g=first(theta,f,...), H=Hfd)     
+  }     
+  list(f=f(theta,...), theta=theta, iter=iter, g=first(theta,f,...), H=Hfd)     
+}
+
+rb <- function(theta,getg=FALSE,k=10) {
+  ## Rosenbrock objective function, suitable for use by â€™bfgsâ€?
+  z <- theta[1]; x <- theta[2]
+  f <- k*(z-x^2)^2 + (1-x)^2 + 1
+  if (getg) {
+    attr(f,"gradient") <- c(2*k*(z-x^2),
+                            -4*k*x*(z-x^2) -2*(1-x))
   }
-    
-
-
+  f
+} ## rb
+theta<-c(-1,2)
+x= bfgs(theta,rb,...,tol=1e-5,fscale=1,maxit=100)
+x
